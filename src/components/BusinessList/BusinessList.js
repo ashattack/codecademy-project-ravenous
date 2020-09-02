b95418d809b6d34f5ff658149194de14416b50d8
ashattack <62859233+ashattack@users.noreply.github.com>
2020-09-01 21:19:22 -0400

    Parts 1 - 2.8

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+class BusinessList extends React.Component {
+    render() {
+        return(
+        <div className="BusinessList">
+            {
+                this.props.business.map((business) => {
+                    return <Business business={business} />;
+                })
+            }
+        </div>)
+    }
+}
+
+export default BusinessList;
